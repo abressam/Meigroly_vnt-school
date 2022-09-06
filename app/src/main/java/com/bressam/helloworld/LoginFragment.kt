43732934ae2a1f215817e485bf9b3fe3d19999c3
Amanda Bressam <71531467+abressam@users.noreply.github.com>
2022-09-06 09:28:30 -0300

    Criando fragment para a tela Home e fragment para a tela Login

---
+++ app/src/main/java/com/bressam/helloworld/LoginFragment.kt
@@ -0,0 +1,11 @@
+package com.bressam.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_login)
+    }
+}
