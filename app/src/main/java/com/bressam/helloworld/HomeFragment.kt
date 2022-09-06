4635be79fb560bc600c4625556e829011d1de175
Amanda Bressam <71531467+abressam@users.noreply.github.com>
2022-09-06 15:29:46 -0300

    Modificando Button para ImageButton

--- app/src/main/java/com/bressam/helloworld/HomeFragment.kt
+++ app/src/main/java/com/bressam/helloworld/HomeFragment.kt
@@ -1,6 +1,22 @@
 package com.bressam.helloworld
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val continueButton = view.findViewById<ImageButton>(R.id.continue_button)
+        continueButton.setOnClickListener { loginFragment() }
+    }
+
+    private fun loginFragment() {
+        parentFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_view, LoginFragment())
+            .addToBackStack(null)
+            .commit()
+    }
 }
